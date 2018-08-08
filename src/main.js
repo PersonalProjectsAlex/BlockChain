f13ca195b2d103c8ee936062c7f6b1fd0160a9d5
Willy Alexander Gonzalez Contreras <alexgonzalez_contreras@outlook.com>
2018-08-07 20:29:15 -0600

    Features/ Post to use a new chain in blockchain is working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 const SHA256 = require("crypto-js/sha256");
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+let upload = multer();
+
+const app = express();
 var datetime = new Date();
+
+app.use(bodyParser.urlencoded({
+        extended: false
+}));
+app.use(bodyParser.json());
+
+const nodeAddr = 'JavaSampleApproach';
 
 class Block {
     constructor(index, timestamp, name, dui, previousHash = '') {
@@ -57,17 +71,50 @@ class Blockchain{
 
 let saveChain = new Blockchain();
 //saveChain.addBlock(new Block(1, "20/07/2017", "Alex", "12345678", { amount: 4 }));
-saveChain.addBlock(new Block(1, datetime, "Willy", "1234567896"));
+/*saveChain.addBlock(new Block(1, datetime, "Willy", "1234567896"));
 saveChain.addBlock(new Block(2, datetime, "Alex", "1234567896"));
-saveChain.addBlock(new Block(3, datetime, "Alejandra", "1234567896"));
-
+saveChain.addBlock(new Block(3, datetime, "Alejandra", "1234567896"));*/
 
 console.log('Blockchain valid? ' + saveChain.isChainValid());
 
 console.log('Changing a block...');
+
+app.post('/transaction', upload.fields([]), function (req, res) {
+   /* const blockIndex = bitcoin.makeNewTransaction(
+        req.body.amount,
+        req.body.sender,
+        req.body.recipient
+    );*/
+
+    req.setEncoding('utf8');
+    saveChain.addBlock(new Block(
+        4, 
+        datetime, 
+        req.body.name, 
+        req.body.DUI
+        )
+    );
+
+    let formData = req.body;
+    console.log('form data', formData);
+
+    
+    const blockIndex = JSON.stringify(saveChain, null, 5);
+    res.json(
+        {
+            message: `Transaction is added to block with index: ${blockIndex}`
+        }
+    );
+});
+
+
 //saveChain.chain[1].name = "sdf";
 // saveChain.chain[1].hash = saveChain.chain[1].calculateHash();
 
-console.log("Blockchain valid? " + saveChain.isChainValid());
+//console.log("Blockchain valid? " + saveChain.isChainValid());
 
  console.log(JSON.stringify(saveChain, null, 4));
+
+app.listen(3000, function () {
+    console.log('> listening on port 3000...');
+});
